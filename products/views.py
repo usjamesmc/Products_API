4ed24aec0ebcdf398a09b79f0e0cabe8bd333e30
usjamesmc <usjamesmc@gmail.com>
2022-04-13 14:37:08 -0500

    Registered Product Model with Admin Site + Completed Post Functionality for Product + Updated Rest Framework Decimal Settings

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def products_list(request):
+
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many = True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+        
--- products_project/settings.py
+++ products_project/settings.py
@@ -68,6 +68,10 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
+
 WSGI_APPLICATION = 'products_project.wsgi.application'
 
 
